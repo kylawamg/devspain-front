3b7c9c373b32565dca2dbe8b6334a0eaa189ce81
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-05-29 13:37:17 +0200

    fix home template and add load-more btn

--- components/posts.js
+++ components/posts.js
@@ -6,13 +6,31 @@ const Posts = ({ posts }) => {
 
   return (
     <div>
+<<<<<<< Updated upstream
       <div
         className="uk-grid uk-grid-match uk-child-width-1-3@s uk-flex"
+=======
+<<<<<<< Updated upstream
+      <div className="uk-child-width-1-2" data-uk-grid>
+        <div>
+          {posts.map((post, i) => {
+            return <Card post={post} key={`post${post.id}`} />;
+          })}
+        </div>
+        <div></div>
+=======
+      <div
+        className="uk-grid uk-grid-match uk-child-width-1-3@s uk-flex-left"
+>>>>>>> Stashed changes
         uk-grid="true"
       >
         {posts.map((post, i) => {
           return <Card post={post} key={`post${post.id}`} />;
         })}
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,23 @@ import POSTS_QUERY from "../apollo/queries/post/posts";
 const Home = () => {
   return (
     <div>
-      <div className="uk-section">
+      <div className="uk-section home-container">
         <div className="uk-container uk-container-large">
+<<<<<<< Updated upstream
           <h2>Últimos articulos</h2>
+=======
+<<<<<<< Updated upstream
+          <h1>Strapi blog</h1>
+=======
+          <h2 class="home-title">Últimos articulos</h2>
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
           <Query query={POSTS_QUERY}>
             {({ data: { posts } }) => {
               return <Posts posts={posts} />;
             }}
           </Query>
+          <button className="uk-button btn-load-more">Ver todos</button>
         </div>
       </div>
     </div>
