6bdfeaa7648f8bdbf49f58c12f0d1821a388991a
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-05-29 14:14:27 +0200

    create footer component and scss file

---
+++ components/footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Link from "next/link";
+import "../assets/scss/footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="uk-container uk-container-large">
+        <div className="">
+          <ul className="uk-subnav uk-subnav-divider" uk-margin="true">
+            <li>
+              <a href="#">Aviso Legal</a>
+            </li>
+            <li>
+              <a href="#">Terminos y condiciones</a>
+            </li>
+            <li>
+              <a href="#">Política de privacidad</a>
+            </li>
+            <li>
+              <a href="#">Contacto</a>
+            </li>
+          </ul>
+          <div>
+            <p>Copyright © Holi. All Rights Reserved.</p>
+          </div>
+          <div>
+            <p>
+              Siguenos en: <span uk-icon="uikit"></span>
+              <span uk-icon="github-alt"></span> <span uk-icon="youtube"></span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
