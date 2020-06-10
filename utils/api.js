85725a2f3d25079296ccd5417a754305bb05ede3
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-06-10 18:00:38 +0200

    testing

--- utils/api.js
+++ utils/api.js
@@ -3,4 +3,5 @@ export function request({ query, variables }) {
   const endpoint = process.env.API_URL;
   const client = new GraphQLClient(endpoint, {});
   return client.request(query, variables);
+  console.log("test");
 }
