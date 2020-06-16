d5d3770c74ce486dd3080d8e6ee55b41d633cfc1
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-06-16 13:07:54 +0200

    change config

--- next.config.js
+++ next.config.js
@@ -1,29 +1,17 @@
 require("dotenv").config();
 const withSass = require("@zeit/next-sass");
 const withFonts = require("next-fonts");
-const webpack = require("webpack");
-
 const isProd = (process.env.NODE_ENV || "production") === "production";
 
-const assetPrefix = isProd ? "/your-repository-name" : "";
 module.exports = withSass({
   env: {
     API_URL: process.env.API_URL,
   },
 });
+
 module.exports = {
   exportPathMap: () => ({
     "/": { page: "/" },
-    "/page1": { page: "/page1" },
   }),
-  assetPrefix: assetPrefix,
-  webpack: (config) => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        "process.env.ASSET_PREFIX": JSON.stringify(assetPrefix),
-      })
-    );
-
-    return config;
-  },
+  assetPrefix: isProd ? "/kylawamg.github.io/devspain-front/" : "",
 };
