3b40620d042b9648567d78f9852c21bab925ba2d
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-06-16 13:10:04 +0200

    rollback config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 require("dotenv").config();
 const withSass = require("@zeit/next-sass");
 const withFonts = require("next-fonts");
-const isProd = (process.env.NODE_ENV || "production") === "production";
 
 module.exports = withSass({
   env: {
     API_URL: process.env.API_URL,
   },
 });
-
-module.exports = {
-  exportPathMap: () => ({
-    "/": { page: "/" },
-  }),
-  assetPrefix: isProd ? "/kylawamg.github.io/devspain-front/" : "",
-};
