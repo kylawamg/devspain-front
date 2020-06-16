2af3ae7d0a56113bb2fbbff92828780cd9e86a6e
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-06-16 13:05:14 +0200

    change css import

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import "../assets/scss/style.scss";
 import Nav from "../components/nav";
 import Footer from "../components/footer";
 
@@ -9,10 +10,6 @@ const App = ({ Component, pageProps, apollo }) => {
       <Head>
         <title>DevSpain blog</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          rel="stylesheet"
-          href={`${this.props.__NEXT_DATA__.assetPrefix}/_next/static/style.css`}
-        />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Staatliches"
