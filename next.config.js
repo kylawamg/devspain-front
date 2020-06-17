80684d7ebc16180b5df374564feea9e70752d8ba
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-06-17 12:26:21 +0200

    add assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const withSass = require("@zeit/next-sass");
 const withFonts = require("next-fonts");
-
+const isProd = process.env.NODE_ENV === "production";
 module.exports = withSass({
   env: {
     API_URL: process.env.API_URL,
+    assetPrefix: isProd ? "https://kylawamg.github.io" : "",
   },
 });
