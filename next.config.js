bc070d225f2a0583268aba8ead8a7a4af1822540
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-06-17 13:26:02 +0200

    regen

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 require("dotenv").config();
 const withSass = require("@zeit/next-sass");
 const withFonts = require("next-fonts");
-const isProd = process.env.NODE_ENV === "production";
-console.log(isProd);
 module.exports = withSass({
   env: {
     API_URL: process.env.API_URL,
-    exportPathMap: () => ({
-      "/": { page: "/" },
-    }),
-    assetPrefix: isProd ? "/devspain-front" : "",
   },
 });
